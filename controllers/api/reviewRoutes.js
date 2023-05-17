0555ac4fde4445edd5a4697b968423f484e81fe0
Francisco <francisco.caldera2015@gmail.com>
2023-05-17 13:43:14 -0500

    added post review in front-end

---
+++ controllers/api/reviewRoutes.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+const withAuth = require('../../utils/auth');
+const { Favorite } = require('../../models');
+
+router.post('/', withAuth, async (req, res) => {
+    try {
+      const newReview = await Favorite.create({
+        game_title: req.body.game_title,
+        rating: req.body.rating,
+        review: req.body.review,
+      });
+  
+      res.status(200).json(newReview);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+
+  module.exports = router
