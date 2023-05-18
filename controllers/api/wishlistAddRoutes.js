cc23ce0565965dd0d5b61db5b64f9f319d244646
Milywayz <jtjenks@gmail.com>
2023-05-18 11:02:49 -0500

    added wishlist fetch for image

--- controllers/api/wishlistAddRoutes.js
+++ controllers/api/wishlistAddRoutes.js
@@ -4,10 +4,22 @@ const { Wishlist } = require('../../models');
 
 router.post('/', withAuth, async (req, res) => {
     try {
+  const key = process.env.API_KEY;
+
+  const url = `https://rawg.io/api/games?search=${req.body.game_title}&page_size=10&page=1&key=${key}`;
+  const options = {
+    method: "GET",
+  };
+
+  
+    const response = await fetch(url, options);
+    const result = await response.json();
+
       const newWishlistAdd = await Wishlist.create({
         game_title: req.body.game_title,
         price: req.body.price,
         review: req.body.review,
+        image: result.results[0].background_image,
         user_id: req.session.user_id,
       });
   
